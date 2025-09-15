88305b88e8a1f7169d2ed1b681c49585931f0553
Muawia24 <amuawia666@gmail.com>
2025-09-15 22:40:29 +0300

    Add commit message generation tool and update index.ts to include it
    - Introduced `generateCommitMessageTool` to create commit messages based on git diffs.
    - Updated `index.ts` to integrate the new tool alongside existing functionality.
    - Enhanced the commit message generation with rules for Conventional Commits style.

--- index.ts
+++ index.ts
@@ -1,16 +1,17 @@
 import { stepCountIs, streamText } from "ai";
 import { google } from "@ai-sdk/google";
 import { SYSTEM_PROMPT } from "./prompts";
-import { getChangesInDirectoryTool } from "./tools";
+import { generateCommitMessageTool, getChangesInDirectoryTool } from "./tools";
 
 
 const CodeReviewAgent = async (prompt: string) => {
     const result = await streamText({
-        model: google("models/gemini-2.5-flash"),
+        model: google("models/gemini-2.5-flash", ),
         prompt,
         system: SYSTEM_PROMPT,
         tools: {
             getChangesInDirectory: getChangesInDirectoryTool,
+            generateCommitMessage: generateCommitMessageTool,
         },
         stopWhen: stepCountIs(10),
     });
--- tools.ts
+++ tools.ts
@@ -1,6 +1,7 @@
-import { tool } from "ai";
+import { tool, generateText } from "ai";
 import { simpleGit } from "simple-git";
 import { z } from "zod";
+import { google } from "@ai-sdk/google";
 
 const excludeFiles = ["dist", "bun.lock"]
 const fileChange = z.object({
@@ -22,9 +23,44 @@ async function getChangesInDirectory({ rootDir }: FileChange) {
     return diffs;
 };
 
+async function generateCommitMessage({ rootDir }: FileChange) {
+    const diffs = await getChangesInDirectory({rootDir});
+    if (!diffs.length) {
+        return "No changes detected — nothing to commit.";
+    }
+    const diffText = diffs
+      .map((d) => `File: ${d.file} \n ${d.diff}`)
+      .join("\n\n");
+
+    const prompt = `
+    You are a commit message generator.
+    Rules:
+    - Use Conventional Commits style (feat, fix, chore, docs, refactor, test, etc.).
+    - Subject line must be <= 72 characters.
+    - Add a short body only if it adds value.
+    - Do not include file paths in the subject.
+
+    Here are the changes:
+    ${diffText}
+    `;
+    const commitMsg = await generateText({
+        model: google("models/gemini-2.5-flash"),
+        prompt,
+    })
+
+    return commitMsg.text;
+}
+
 
 export const getChangesInDirectoryTool = tool({
     description: "Get the git diffs for all changed files in a directory",
     inputSchema: fileChange,
     execute: getChangesInDirectory,
+});
+
+export const generateCommitMessageTool = tool({
+    description: "Generate a commit message from git diffs",
+    inputSchema: fileChange,
+    execute: generateCommitMessage,
+
 });
