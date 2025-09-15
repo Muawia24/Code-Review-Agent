1557a73ca1f15f887877d73b85bb054265bd0c77
Muawia24 <amuawia666@gmail.com>
2025-09-15 13:35:15 +0300

    Add initial project setup with TypeScript, dependencies, and configuration files
    - Created .gitignore to exclude unnecessary files and directories.
    - Added bun.lock for dependency management.
    - Implemented index.ts as the main entry point with a sample usage of the AI SDK.
    - Defined package.json with project metadata and dependencies.
    - Included prompts.ts for system prompt configuration.
    - Added README.md for project instructions.
    - Configured tsconfig.json for TypeScript compiler options.

--- index.ts
+++ index.ts
@@ -1,10 +1,24 @@
-import { generateText } from "ai";
+import { stepCountIs, streamText } from "ai";
 import { google } from "@ai-sdk/google";
+import { SYSTEM_PROMPT } from "./prompts";
+import { getChangesInDirectoryTool } from "./tools";
 
 
-const { text } = await generateText({
-    model: google("models/gemini-2.5-flash"),
-    prompt: "What is an AI agent?",
-})
+const CodeReviewAgent = async (prompt: string) => {
+    const result = await streamText({
+        model: google("models/gemini-2.5-flash"),
+        prompt,
+        system: SYSTEM_PROMPT,
+        tools: {
+            getChangesInDirectory: getChangesInDirectoryTool,
+        },
+        stopWhen: stepCountIs(10),
+    });
 
-console.log(text);
+    for await (const chunk of result.textStream){
+        process.stdout.write(chunk);
+    }
+    
+}
+
+await CodeReviewAgent("Review the code changes in '../my-agent' directory, make your reviews and suggestions file by file");
